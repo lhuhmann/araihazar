fd704a175a23db3bd999d1e91ce8685cf171e45d
Britt Huhmann <bhuhmann@gmail.com>
2020-01-14 22:00:20 -0500

    Add plots for binned data and code to calculate water and urine fractions

--- linear_regression.py
+++ linear_regression.py
@@ -47,10 +47,10 @@ def simple_regress(data, group_name):
             '$\n$slope = ' + str(round(results.params[1], 2)) + \
             '$\n$intercept = $' + str(int(results.params[0])),
             loc=2, frameon=False))
-    plt.savefig(group_name + '_simple_regression.png')
+    plt.savefig('plots/' + group_name + '_simple_regression.png')
 
     data['urine_as_pred_simple'] = urine_as_pred
-    return data
+    return data, results
 
 #%% add wells in family compound
 def two_slope_regress(data, group_name):
@@ -77,7 +77,7 @@ def two_slope_regress(data, group_name):
             '$\n$slope_{PrimaryWell} = ' + str(round(results.params[1], 2)) + \
             '$\n$intercept = $' + str(int(results.params[0])),
             loc=2, frameon=False))
-    plt.savefig(group_name + '_two_slope_regression_primary_well.png')
+    plt.savefig('plots/' + group_name + '_two_slope_regression_primary_well.png')
 
     fig, ax = plt.subplots(figsize=(8,6))
     ax.set_xlabel(r'Average Arsenic of Wells within 50 m ($\mu g/L$)', fontsize=16)
@@ -91,10 +91,10 @@ def two_slope_regress(data, group_name):
             '$\n$slope_{50mWellAverage} = ' + str(round(results.params[1], 2)) + \
             '$\n$intercept = $' + str(int(results.params[0])),
             loc=2, frameon=False))
-    plt.savefig(group_name + '_two_slope_regression_compound_wells.png')
+    plt.savefig('plots/' + group_name + '_two_slope_regression_compound_wells.png')
 
     data['urine_as_pred_multiple'] = urine_as_pred
-    return data
+    return data, results
 
 # #%% add wells beyond family compound
 # def three_slope_regress(data, group_name):
@@ -131,13 +131,13 @@ def two_slope_regress(data, group_name):
 subset_data = data[data['knew_well_as'] == False]
 
 # 1. participants who did not know their well As
-subset_data_with_pred_simple = simple_regress(subset_data, 'subset')
-subset_data_with_pred_both = two_slope_regress(subset_data_with_pred_simple, 'subset')
+subset_data_with_pred_simple, subset_model_simple = simple_regress(subset_data, 'subset')
+subset_data_with_pred_both, subset_model_multiple = two_slope_regress(subset_data_with_pred_simple, 'subset')
 # three_slope_regress(subset_data, 'subset')
 
 # 2. all participants
-all_data_with_pred_simple = simple_regress(data, 'all')
-all_data_with_pred_both = two_slope_regress(data, 'all')
+all_data_with_pred_simple, all_model_simple = simple_regress(data, 'all')
+all_data_with_pred_both, all_model_multiple = two_slope_regress(data, 'all')
 # three_slope_regress(data, 'all')
 
 #%% get the binned plots
@@ -163,7 +163,7 @@ def get_binned_data(data, nbins):
     #print(data.head(5))
     #print(data.index.is_monotonic_increasing)
     data = data.reset_index(drop=True)
-    data = data.sort_values(by=['urine_as'])
+    data = data.sort_values(by=['arsenic_ugl'])
     data = data.reset_index(drop=True)
     binned_data = pd.DataFrame()
     n_tot = data.shape[0]
@@ -180,13 +180,110 @@ def get_binned_data(data, nbins):
     last_index = n_tot
     print(n_tot, first_index, last_index)
     binned_data = add_values(data, binned_data, first_index, last_index)
-    print(binned_data.head(5))
+    print(binned_data.columns)
     return binned_data
+
+def plot_binned(data, group_name, xvar, yvar, xmax, ymax, xlabel):
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.set_ylabel(r'Urinary Arsenic ($\mu g/L$)', fontsize=16)
+    ax.set_xlabel(xlabel + r' ($\mu g/L$)', fontsize=16)
+    ax.errorbar(data[xvar + '_mean'], data.urine_as_mean, 
+                yerr=data.urine_as_sem, xerr=data[xvar + '_sem'],
+                fmt='o', markersize=5, mfc='k', mec='none', ecolor='k', capsize=2)
+    ax.errorbar(data[xvar + '_mean'], data[yvar+'_mean'], 
+                yerr=data[yvar+'_sem'], xerr=data[xvar+'_sem'],
+                fmt='o', markersize=5, mfc='r', mec='none', ecolor='r', capsize=2)
+    ax.set_xlim([0,xmax])
+    ax.set_ylim([0,ymax])
+    ax.xaxis.set_ticks([0, 100, 200, 300, 400])
+    ax.yaxis.set_ticks([0, 100, 200, 300, 400])
+    ax.tick_params(axis='both', labelsize=16)
+    plt.savefig('plots/' + group_name + '_' + xvar + '_' + yvar + '_binned.png')
         
 # binned urine as vs primary well As for subset
-subset_binned_data = get_binned_data(subset_data_with_pred_both, 10)
+subset_binned_data = get_binned_data(subset_data_with_pred_both, 15)
+plot_binned(subset_binned_data, 'subset', 'arsenic_ugl', 'urine_as_pred_simple', 
+            400, 400, 'Primary Household Well Arsenic')
+plot_binned(subset_binned_data, 'subset', 'arsenic_ugl', 'urine_as_pred_multiple', 
+            400, 400, 'Primary Household Well Arsenic')
+plot_binned(subset_binned_data, 'subset', 'other_as_50m', 'urine_as_pred_multiple',
+            400, 400, 'Average Arsenic of Wells within 50 m')
 
 # binned urine as vs primary well As for all
-all_binned_data = get_binned_data(all_data_with_pred_both, 10)
+all_binned_data = get_binned_data(all_data_with_pred_both, 15)
+plot_binned(all_binned_data,'all', 'arsenic_ugl', 'urine_as_pred_simple', 
+            400, 400, 'Primary Household Well Arsenic')
+plot_binned(all_binned_data,'all', 'arsenic_ugl', 'urine_as_pred_multiple', 
+            400, 400, 'Primary Household Well Arsenic')
+plot_binned(all_binned_data, 'all', 'other_as_50m', 'urine_as_pred_multiple',
+            400, 400, 'Average Arsenic of Wells within 50 m')
+
+#%% solve for fu and fp
+# simple linear regression, distributed well model
+
+# constant parameters
+# mass fraction of water consumed via food
+ff = 0.2
+# mass fraction of water produced from cellular respiration
+fc = 0.12
+# mass fraction of arsenic lost to defection, 
+md = 0.06
+# mass fraction of arsenic lost to a sink in the body
+mb = 0
+# Mf/Q, mass of arsenic consumed via food, ug/L
+MfQ = 21
+# average arsenic of all private wells in the data set (proxy for all wells in the study area), ug/L
+avgAs = 95
+
+def solve_fracs_simple(model):
+    # slope (primary well arsenic)
+    slope = model.params[1]
+    # intercept
+    intercept = model.params[0]
+
+    # solve for fp, fu, and fo
+    fp = (slope*(1-ff-fc)*avgAs+MfQ)/(slope*avgAs+intercept)
+    fu = (1-md-mb)*(fp/slope)
+    fo = 1 - fp - ff - fc
+
+    frac_primary_well = fp/(fp+fo)
+    frac_other_well = fo/(fp+fo)
+    return {'fu':fu, 'fp':fp, 'fo':fo, 
+            'frac_primary_well':frac_primary_well, 
+            'frac_other_well':frac_other_well}
+
+def solve_fracs_multiple(model):
+    # slope (primary well arsenic)
+    slope_primary = model.params[1]
+    # slope (neighbor well arsenic)
+    slope_neighbor = model.params[2]
+    # intercept
+    intercept = model.params[0]
+
+    # solve for fp, fu, fo, fn
+    fu = (1 - md - mb)*(1 - ff - fc + MfQ/avgAs)/(slope_primary + slope_neighbor + intercept/avgAs)
+    fp = slope_primary*(1 - ff - fc + MfQ/avgAs)/(slope_primary + slope_neighbor + intercept/avgAs)
+    fo = intercept*(1 - ff - fc + MfQ/avgAs)/(slope_primary*avgAs + slope_neighbor*avgAs + intercept) - MfQ/avgAs
+    fn = 1 - fp - fo - ff - fc
+    frac_primary_well = fp/(fp+fo+fn)
+    frac_other_well = fo/(fp+fo+fn)
+    frac_neighbor_well = fn/(fp+fo+fn)
+    return {'fu':fu, 'fp':fp, 'fo':fo, 'fn':fn,
+            'frac_primary_well':frac_primary_well, 
+            'frac_other_well':frac_other_well,
+            'frac_neighbor_well':frac_neighbor_well}
+
+subset_simple_fracs = solve_fracs_simple(subset_model_simple)
+print(subset_simple_fracs)
+subset_multiple_fracs = solve_fracs_multiple(subset_model_multiple)
+print(subset_multiple_fracs)
+
+all_simple_fracs = solve_fracs_simple(all_model_simple)
+print(all_simple_fracs)
+all_multiple_fracs = solve_fracs_multiple(all_model_multiple)
+print(all_multiple_fracs)
+
+
+
 
 
