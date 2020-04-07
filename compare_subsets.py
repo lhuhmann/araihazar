fa4cd4f8e1db322a50aaf1cd241d0eae3df8fa26
Britt Huhmann <bhuhmann@gmail.com>
2020-04-07 14:25:23 -0400

    Compare water arsenic in addition to comparing urine arsenic for two subsets of participants

--- compare_subsets.py
+++ compare_subsets.py
@@ -23,12 +23,12 @@ def compare_subsets(distributed_results, data):
     # hypothesis: for the people with primary well arsenic greater than 250 ug/L, 
     # the group that may have been informed of their well arsenic has lower urinary 
     # arsenic than the group that was not informed
-    compare_means(not_know_subset, may_know_subset, arsenic_ugl_lower_bound=300, arsenic_ugl_upper_bound=100000)
+    compare_well_and_urine_as_means(not_know_subset, may_know_subset, arsenic_ugl_lower_bound=250, arsenic_ugl_upper_bound=100000)
 
     # hypothesis: for the people with primary well arsenic less than 10 ug/L,
     # the group that may have been informed of their well arsenic has lower urinary 
     # arsenic than the group that was not informed
-    compare_means(not_know_subset, may_know_subset, arsenic_ugl_lower_bound=100, arsenic_ugl_upper_bound=200)
+    compare_well_and_urine_as_means(not_know_subset, may_know_subset, arsenic_ugl_lower_bound=0, arsenic_ugl_upper_bound=10)
 
 
 def make_subset(data, group_name): # pylint: disable=too-many-return-statements
@@ -67,10 +67,10 @@ def plot_group_comparison(results, not_know_subset, may_know_subset, plotname, x
     # as discussed in https://github.com/matplotlib/matplotlib/issues/409
     ax.errorbar(binned_data_not_know['arsenic_ugl_mean'], binned_data_not_know['urine_as_mean'],
                 xerr=binned_data_not_know['arsenic_ugl_sem'], yerr=binned_data_not_know['urine_as_sem'],
-                fmt='ok', markersize=10, mfc='k', mec='none', ecolor='k', capsize=2, zorder=1)
+                fmt='ok', linestyle='-.', markersize=10, mfc='k', mec='none', ecolor='k', capsize=2, zorder=1)
     ax.errorbar(binned_data_may_know['arsenic_ugl_mean'], binned_data_may_know['urine_as_mean'],
                 xerr=binned_data_may_know['arsenic_ugl_sem'], yerr=binned_data_may_know['urine_as_sem'],
-                fmt='ob', markersize=10, mfc='b', mec='none', ecolor='b', capsize=2, zorder=1)
+                fmt='ob', linestyle='-.', markersize=10, mfc='b', mec='none', ecolor='b', capsize=2, zorder=1)
     ax.plot(np.linspace(0, 1000, 100), np.linspace(0, 1000, 100)*results.params[1] + results.params[0],
                         'r', linewidth=3)
     ax.set_xlim([0, xmax])
@@ -80,28 +80,37 @@ def plot_group_comparison(results, not_know_subset, may_know_subset, plotname, x
     ax.tick_params(axis='both', labelsize=16)
     plt.savefig(f'plots/{plotname}.png')
 
-def compare_means(not_know_subset, may_know_subset, arsenic_ugl_lower_bound, arsenic_ugl_upper_bound):
-    """For the given data subsets, within the given bounds, checks if the first data subset has a 
-    significantly higher mean urinary arsenic than the second data subset."""
-    not_know_subset = not_know_subset[(not_know_subset.arsenic_ugl > arsenic_ugl_lower_bound) & 
+def compare_well_and_urine_as_means(not_know_subset, may_know_subset, arsenic_ugl_lower_bound, arsenic_ugl_upper_bound):
+    """For the given data subsets, within the given bounds, checks if the first data subset has a
+    significantly higher mean well arsenic and mean urinary arsenic than the second data subset."""
+    not_know_subset = not_know_subset[(not_know_subset.arsenic_ugl > arsenic_ugl_lower_bound) &
                                       (not_know_subset.arsenic_ugl < arsenic_ugl_upper_bound)]
-    may_know_subset = may_know_subset[(may_know_subset.arsenic_ugl > arsenic_ugl_lower_bound) & 
+    may_know_subset = may_know_subset[(may_know_subset.arsenic_ugl > arsenic_ugl_lower_bound) &
                                       (may_know_subset.arsenic_ugl < arsenic_ugl_upper_bound)]
-    test_statistic, p_value = scipy.stats.bartlett(not_know_subset.urine_as, may_know_subset.urine_as)
+    urine_comparison = compare_means(not_know_subset, may_know_subset, 'urine_as')
+    well_comparison = compare_means(not_know_subset, may_know_subset, 'arsenic_ugl')
+    with open(os.path.abspath(f'../araihazar-data/analysis_output/well_as_{arsenic_ugl_lower_bound}_to_{arsenic_ugl_upper_bound}_well_and_urine_as_comparison.csv'), "w") as savefile:
+        writer = csv.writer(savefile)
+        writer.writerow(['mean compared', 'urine equal variance', 'urine test statistic', 'urine one-tailed p-value'])
+        writer.writerow(['urine'] + urine_comparison)
+        writer.writerow(['well water'] + well_comparison)
+
+def compare_means(not_know_subset, may_know_subset, to_compare):
+    """Takes two subsets of study participants and a category to compare. Returns whether an equal or unequal
+    variance t-test was conducted, along with the test statistic and the one-tailed p-value for the test."""
+    test_statistic, p_value = scipy.stats.bartlett(not_know_subset[to_compare], may_know_subset[to_compare])
     if p_value < 0.05:
         equal_var = False
     else:
         equal_var = True
     # do a two-tailed t-test
-    test_statistic, p_value_two_tailed = scipy.stats.ttest_ind(not_know_subset.urine_as,
-                                                               may_know_subset.urine_as, equal_var=equal_var)
+    test_statistic, p_value_two_tailed = scipy.stats.ttest_ind(not_know_subset[to_compare],
+                                                               may_know_subset[to_compare],
+                                                               equal_var=equal_var)
     # we want a one-tailed t-test, since we are specifically testing if may_know_subset has a lower
     # mean urinary arsenic, so divide p_value from two-tailed t-test by 2
     # (https://stats.idre.ucla.edu/other/mult-pkg/faq/general/faq-what-are-the-differences-between-one-tailed-and-two-tailed-tests/) 
     p_value_one_tailed = p_value_two_tailed/2
     # we expect a positive test statistic if mean of not_know_subset is greater than mean of may_know_subset
     print(equal_var, test_statistic, p_value_one_tailed)
-    with open(os.path.abspath(f'../araihazar-data/analysis_output/well_as_{arsenic_ugl_lower_bound}_to_{arsenic_ugl_upper_bound}_urine_as_comparison.csv'), "w") as savefile:
-        writer = csv.writer(savefile)
-        writer.writerow(['equal variance', 'test statistic', 'one-tailed p-value'])
-        writer.writerow([equal_var, test_statistic, p_value_one_tailed])
+    return [equal_var, test_statistic, p_value_one_tailed]
